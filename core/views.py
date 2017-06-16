7395063b684b4907df26032561c6eb1e39963a0a
shivam <tamyworld@gmail.com>
2017-06-16 17:56:32 +0530

    rectification

--- core/views.py
+++ core/views.py
@@ -100,7 +100,7 @@ def TaskView(request):
         assigned = User.objects.filter(is_staff=True)
         return render(request, "task.html", {"tasks": records,'projects':projects,"worktype":worktypes,"assigned_by":assigned})
     if request.method == 'POST':
-        task=Task(**{"name":request.POST.get("name"),"description":request.POST.get("description"),"starttime":request.POST.get("starttime"),"endtime":request.POST.get("endtime"),"project":Project.objects.get(pk=request.POST.get("project")),"worktype":WorkType.objects.get(pk=request.POST.get("worktype")),"assigned_by":request.user,"user":request.user})
-        task.save
+        task=Task(**{"name":request.POST.get("name"),"description":request.POST.get("description"),"starttime":request.POST.get("starttime"),"endtime":request.POST.get("endtime"),"project":Project.objects.get(pk=request.POST.get("project")),"worktype":WorkType.objects.get(pk=request.POST.get("worktype")),"assigned_by":User.objects.get(pk=request.POST.get("assigned_by")),"user":request.user})
+        task.save()
 
         return render(request, "viewtasks.html")
