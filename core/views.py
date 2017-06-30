3aba76bbdb0420357a6ed39c7fbf0d05392c37e4
shivam <tamyworld@gmail.com>
2017-06-30 15:15:04 +0530

    minor fixes

--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, user_passes_test
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout
 from .models import *
