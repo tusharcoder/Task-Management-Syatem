204f9deffb78446b5d52cc1d100332925ba18fa7
shivam <shivam.srivastava8322@gmail.com>
2017-07-04 17:24:18 +0530

    merge settings

--- taskmgt/settings.py
+++ taskmgt/settings.py
@@ -43,8 +43,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core.apps.CoreConfig',
-    'github_hook',
+    'core.apps.CoreConfig'
 )
 
 MIDDLEWARE_CLASSES = (
